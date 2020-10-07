77aa639ff003c7f3c5d922482755f8822bc2f685
garik911 <i-g-o-r911@mail.ru>
2020-10-07 23:02:44 +0300

    Peugeot2

--- practica.py
+++ practica.py
@@ -2,9 +2,9 @@ print('привет коллеги кодеры')
 Peugeot = 406
 Peugeot
 print(Peugeot)
-def car ():
+def car (Peugeot):
     if car == 406:
         print('no')
     else:
         print('OK')
-    car(Peugeot)
+car(Peugeot)
